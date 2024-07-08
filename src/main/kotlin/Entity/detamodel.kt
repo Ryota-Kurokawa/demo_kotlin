d40249ce664c011ca7b9f937346b73b4208c12a4
ryota <kurokawa.ryota.1124@gmail.com>
2024-07-08 11:41:57 +0900

    json

--- src/main/kotlin/Entity/detamodel.kt
+++ src/main/kotlin/Entity/detamodel.kt
@@ -1,1 +1,2 @@
 data class HelloResponse(val message: String)
+data class HelloRequest(val name: String)
--- src/main/kotlin/com/example/demo/HelloController.kt
+++ src/main/kotlin/com/example/demo/HelloController.kt
@@ -1,12 +1,10 @@
 package com.example.demo
 
+import HelloRequest
 import HelloResponse
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Controller
 @RequestMapping("hello")
@@ -24,5 +22,15 @@ class GreeterController {
     @GetMapping("/hello")
     fun hello(@RequestParam("name") name: String): HelloResponse {
         return HelloResponse("hello ${name}")
+    }
+
+    @GetMapping("/hello/{name}")
+    fun helloPathValue(@PathVariable("name") name: String): HelloResponse {
+        return HelloResponse("Hello $name")
+    }
+
+    @PostMapping("/hello")
+    fun helloByPost(@RequestBody request: HelloRequest): HelloResponse {
+        return HelloResponse("Hello ${request.name}")
     }
 }
